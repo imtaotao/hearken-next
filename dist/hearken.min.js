3fc597c4fc36fe32501c07f2332b4b725c7e05d7
jianglirui <535904200@qq.com>
2020-06-23 18:43:08 +0800

    chore:add dist

---
+++ dist/hearken.min.js
@@ -0,0 +1,17 @@
+/*!
+ * Hearken.js v0.0.1
+ * (c) 2020-2020 Imtaotao and JiangLiruii
+ * Released under the MIT License.
+ */
+'use strict';
+
+function aa(d) { }
+
+var index = {
+    a: function (p) {
+        console.log(p);
+    },
+    d: aa,
+};
+
+module.exports = index;
