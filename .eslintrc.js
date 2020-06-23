6012e570559489383cac3a6a5f2453be87d67bde
imtaochen <imtaochen@tencent.com>
2020-06-23 21:13:24 +0800

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+const DOMGlobals = ['window', 'document']
+const NodeGlobals = ['module', 'require']
+
+// eslint-disable-next-line no-restricted-globals
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': [
+      'error',
+      // we are only using this rule to check for unused arguments since TS
+      // catches unused variables but not args.
+      { varsIgnorePattern: '.*', args: 'after-used', argsIgnorePattern: '^_' }
+    ],
+    // most of the codebase are expected to be env agnostic
+    'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+    // since we target ES2015 for baseline support, we need to forbid object
+    // rest spread usage (both assign and destructure)
+    'no-restricted-syntax': [
+      'error',
+      'ObjectExpression > SpreadElement',
+      'ObjectPattern > RestElement'
+    ],
+    'no-var': 2,
+    'camelcase': 2,
+    'indent': [2, 2],
+    'func-style': [0, 'declaration'],
+    'quotes': ['error', 'single'],
+    'no-use-before-define': 2,
+    'newline-before-return': 2,
+  },
+  overrides: [
+    // tests, no restrictions (runs in Node / jest with jsdom)
+    {
+      files: ['test/*'],
+      rules: {
+        'no-restricted-globals': 'off',
+        'no-restricted-syntax': 'off'
+      }
+    },
+    // build script
+    {
+      files: ['script/*'],
+      rules: {
+        'no-restricted-globals': 'off',
+        'no-restricted-syntax': 'off'
+      }
+    }
+  ]
+}
