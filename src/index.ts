926acb41dd713a7d31037dd8014eb4a2aa964331
imtaochen <imtaochen@tencent.com>
2020-06-29 19:59:40 +0800

    feat: add utils methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as _ from './share/index'
+
 export const sum = (num1: number, num2: number) => {
   if (__DEV__) {
     console.log('test')
@@ -6,4 +8,4 @@ export const sum = (num1: number, num2: number) => {
   return num1 + num2
 }
 
-export default { sum }
+export const u = _
