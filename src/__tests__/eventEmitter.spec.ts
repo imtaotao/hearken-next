a5a5c4c68ea68f738cd00ac1ef2c685430ef0aa7
jianglirui <535904200@qq.com>
2020-06-30 11:00:23 +0800

    chore(share): improve coverage to 100%

--- src/__tests__/eventEmitter.spec.ts
+++ src/__tests__/eventEmitter.spec.ts
@@ -1,4 +1,5 @@
 import { eventEmitter } from '../share/eventEmitter'
+import { doesNotMatch } from 'assert'
 
 describe('test EventEmitter', () => {
   test('test "on" functionality', done => {
@@ -13,13 +14,41 @@ describe('test EventEmitter', () => {
     eventEmitter.emit('jest')
     eventEmitter.emit('jest')
   })
-  test('test "once" functionality', () => {
-    eventEmitter.on('once', () => {
+  test('test "once" functionality', done => {
+    eventEmitter.once('once', () => {
       console.log('once')
     })
     expect(eventEmitter.emit('once')).toEqual(true)
     setTimeout(() => {
       expect(eventEmitter.emit('once')).toEqual(false)
+      done()
     }, 1000)
+  })
+  test('remove error eventName', () => {
+    expect(eventEmitter.remove('test', () => {})).toEqual(false)
+  })
+  test('remove error fn', () => {
+    eventEmitter.on('test', () => {})
+    expect(eventEmitter.remove('test', () => {})).toEqual(false)
+  })
+  test('test "remove" functionality', () => {
+    const test1 = () => {}
+    const test2 = () => {}
+    eventEmitter.on('test', test1)
+    eventEmitter.on('test', test2)
+    expect(eventEmitter.emit('test')).toEqual(true)
+    expect(eventEmitter.remove('test', test1)).toEqual(true)
+    expect(eventEmitter.emit('test')).toEqual(true)
+    expect(eventEmitter.remove('test', test2)).toEqual(true)
+    expect(eventEmitter.emit('test')).toEqual(false)
+  })
+  test('test "removeAll" functionality', () => {
+    const test1 = () => {}
+    const test2 = () => {}
+    eventEmitter.on('test', test1)
+    eventEmitter.on('test', test2)
+    expect(eventEmitter.emit('test')).toEqual(true)
+    expect(eventEmitter.removeAll('test')).toEqual(true)
+    expect(eventEmitter.emit('test')).toEqual(false)
   })
 })
--- src/share/eventEmitter.ts
+++ src/share/eventEmitter.ts
@@ -7,24 +7,46 @@ class EventEmitter {
       this.liseners[eventName] = [callback]
     }
   }
+
   once(eventName: string, callback: Function) {
     const callOnce = () => {
       callback()
-      const currentListeners = this.liseners[eventName]
-      currentListeners.splice(currentListeners.indexOf(callback))
+      this.remove(eventName, callOnce)
     }
     this.on(eventName, callOnce)
   }
+  private checkListenerEmpty(listeners: any[]) {
+    if (!listeners || listeners.length === 0) {
+      return true
+    }
+    return false
+  }
   emit(eventName: string, ...data: any[]) {
     const currentListeners = this.liseners[eventName]
-    if (!currentListeners || currentListeners.length === 0) {
-      return null
+    if (this.checkListenerEmpty(currentListeners)) {
+      return false
     }
     this.liseners[eventName].map(item => {
       setTimeout(() => {
         item(...data)
       })
     })
+    return true
+  }
+  remove(eventName: string, fn: Function) {
+    const currentListeners = this.liseners[eventName]
+    if (this.checkListenerEmpty(currentListeners)) {
+      return false
+    }
+    const fnIndex = currentListeners.indexOf(fn)
+    if (!~fnIndex) {
+      return false
+    }
+    currentListeners.splice(fnIndex, 1)
+    return true
+  }
+  removeAll(eventName: string) {
+    this.liseners[eventName] = []
     return true
   }
 }
