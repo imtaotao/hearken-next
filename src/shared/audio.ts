b780a8a09a11591988cecc373981f67e7894ffdd
imtaochen <imtaochen@tencent.com>
2020-07-02 16:39:44 +0800

    feat: add node utils

--- src/shared/audio.ts
+++ src/shared/audio.ts
@@ -1,3 +1,8 @@
+const MEDIA_ELEMENT_NODES = new WeakMap<
+  HTMLAudioElement,
+  MediaElementAudioSourceNode
+>()
+
 export interface ExtendAudioContext {
   $isClosed: boolean
 }
@@ -5,14 +10,45 @@ export interface ExtendAudioContext {
 export function createAudioContext(
   obj: any,
 ): AudioContext & ExtendAudioContext {
-  if (!obj.$AudioCtx) {
-    obj.$AudioCtx = new (window.AudioContext ||
+  if (!obj.$ctx) {
+    obj.$ctx = new (window.AudioContext ||
       (window as any).webkitAudioContext ||
       (window as any).mozAudioContext ||
       (window as any).msAudioContext)()
   }
 
-  obj.$AudioCtx.$isClosed = false
+  obj.$ctx.$isClosed = false
+  return obj.$ctx
+}
 
-  return obj.$AudioCtx
+export function createFilter(ctx: AudioContext) {
+  const ra = ctx.createBiquadFilter()
+  ra.type = 'peaking'
+  return ra
+}
+
+export function createAnalyser(ctx: AudioContext, fftSize: number) {
+  const analyser = ctx.createAnalyser()
+  // fourier transform parameter
+  analyser.fftSize = fftSize * 2
+  return analyser
+}
+
+export function createGainNode(ctx: AudioContext): GainNode {
+  return (ctx as any)[
+    (ctx.createGain as Function) ? 'createGain' : 'createGainNode'
+  ]()
+}
+
+export function createMediaElementSource(
+  ctx: AudioContext,
+  el: HTMLAudioElement,
+) {
+  if (MEDIA_ELEMENT_NODES.has(el)) {
+    return MEDIA_ELEMENT_NODES.get(el) as MediaElementAudioSourceNode
+  } else {
+    const source = ctx.createMediaElementSource(el)
+    MEDIA_ELEMENT_NODES.set(el, source)
+    return source
+  }
 }
