476992a1220230d58385ae8c9c3f90dfbfc98d46
imtaochen <imtaochen@tencent.com>
2020-07-02 19:08:34 +0800

    feat: add manager code

--- src/manager/audioStream.ts
+++ src/manager/audioStream.ts
@@ -1,7 +1,5 @@
 import { Manager } from './runtime'
 
 export function loadAudioSource(manager: Manager) {
-  if (manager.model === 'link' && typeof manager.buffer === 'string') {
-    console.log(manager)
-  }
+  console.log(manager)
 }
