8655eeade3cea92d17234800d2b4035c6d4e8241
imtaochen <imtaochen@tencent.com>
2020-07-06 20:13:33 +0800

    feat: add connect method

--- src/manager/runtime.ts
+++ src/manager/runtime.ts
@@ -1,40 +1,16 @@
-import { assert } from '../shared/index'
 import { createContext } from './context'
+import { assert, isAudioNode } from '../shared/index'
 import { extendEvent } from '../shared/eventEmitter'
 
 interface ManagerOptions {}
 
 type Model = 'link' | 'buffer' | 'void'
-type AudioNodeFn = (audioNode: AudioNode) => AudioNode
-type AudioNodeWrap = (manager: Manager['$context']) => AudioNodeFn
+type AudioNodeFn = (prevAudioNode: AudioNode | null) => AudioNode
+type AudioNodeWrap = (context: Manager['$context']) => AudioNodeFn
 
 // Need filter manager options
 function checkOptions(options: ManagerOptions) {
   return options || {}
-}
-
-// Connect audio node
-function connect(this: Manager, registrar: AudioNodeWrap) {
-  if (__DEV__) {
-    assert(typeof registrar === 'function', '`registrar` is not a function.')
-  }
-
-  const fn = registrar(this.$context)
-  if (__DEV__) {
-    assert(
-      typeof fn === 'function',
-      'The registrar should return a function, ' +
-        'like this: \n\n' +
-        `
-          manager.connect(context => {
-            return audioNode => {
-              context.AudioContext.createBiquadFilter()
-            }
-          })
-        `,
-    )
-  }
-  this.nodes.add(fn)
 }
 
 // Close audio context, free up resources
@@ -83,19 +59,73 @@ function loaded(this: Manager) {
   return this.$loaded
 }
 
+// Connect audio node
+function registrar(this: Manager, registrar: AudioNodeWrap) {
+  if (__DEV__) {
+    assert(typeof registrar === 'function', '`registrar` is not a function.')
+  }
+
+  const fn = registrar(this.$context)
+  if (__DEV__) {
+    assert(
+      typeof fn === 'function',
+      'The registrar should return a function, ' +
+        'like this: \n\n' +
+        `
+          manager.registrar(context => {
+            return audioNode => {
+              context.AudioContext.createBiquadFilter()
+            }
+          })
+        `,
+    )
+  }
+  this.nodes.add(fn)
+}
+
+function connect(this: Manager) {
+  // We need collect nodes to use when disconnected
+  let prevNode: AudioNode | null = null
+  const ctx = this.$context.audioContext
+  const nodes: AudioNode[] = [ctx.destination]
+
+  this.connect.emit()
+  this.nodes.forEach((fn) => {
+    const curNode = fn(prevNode)
+    if (__DEV__) {
+      assert(isAudioNode(curNode), 'Should return an audioNode.')
+    }
+    prevNode = curNode
+    nodes.push(curNode)
+  })
+
+  // connect nodes
+  for (let i = nodes.length - 2; i === 0; i--) {
+    const curNode = nodes[i]
+    if (!curNode) break
+    ;((prevNode as unknown) as AudioNode).connect(curNode)
+    prevNode = curNode
+  }
+
+  // stash nodes
+  this.audioNodes = nodes
+}
+
 export class Manager {
   private $options: ManagerOptions
   public $model: Model = 'void'
   public $loaded = false
+  public audioNodes: AudioNode[] = []
   public nodes = new Set<AudioNodeFn>()
   public $context = createContext(this)
   public $plugins: { [key: string]: any } = {}
 
   // methods
   public apply = apply
-  public connect = extendEvent(connect)
+  public registrar = registrar
   public close = extendEvent(close)
   public loaded = extendEvent(loaded)
+  public connect = extendEvent(connect)
 
   constructor(options: ManagerOptions) {
     this.$options = checkOptions(options)
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -27,6 +27,10 @@ export function isAudioBuffer(val: unknown) {
   return Object.prototype.toString.call(val) === '[object AudioBuffer]'
 }
 
+export function isAudioNode(val: AudioNode) {
+  return /\[object\s{1}.*Node\]/.test(Object.prototype.toString.call(val))
+}
+
 export function remove<T>(list: Array<T>, el: T) {
   const i = list.indexOf(el)
   if (~i) {
