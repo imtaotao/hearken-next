bada346090eb11dc1feb9154ae3b29ff08308b1a
imtaochen <imtaochen@tencent.com>
2020-07-07 17:13:49 +0800

    feat: add apis

--- src/plugins/long.ts
+++ src/plugins/long.ts
@@ -1,5 +1,5 @@
+import { assert } from '../shared/index'
 import { Manager } from '../manager/runtime'
-import { warn, assert } from '../shared/index'
 import { extend, ExtendFn } from 'src/shared/eventEmitter'
 
 interface StartOptions {
@@ -10,57 +10,83 @@ interface StartOptions {
 
 interface Player {
   manager: Manager
+  options?: StartOptions
   playing: () => boolean
   el: HTMLAudioElement | null
   close: ExtendFn<() => void>
   add: ExtendFn<(url: string) => void>
 }
 
-function playSuccess(player: Player) {
-  return () => {
-    player.add.emit()
-    return true
+// Use audio element load source and control audio behavior
+function add(this: Player, url: string) {
+  if (__DEV__) {
+    assert(typeof url === 'string', 'error')
+  }
+  const opts = this.options || {}
+  const el = (this.el = opts.audio || new Audio())
+
+  el.src = url
+  el.loop = Boolean(opts.loop)
+  if (opts.crossOrigin) {
+    el.crossOrigin = opts.crossOrigin
+  }
+  el.oncanplay = () => {
+    // Alter context canplay attribute and dispatch event
+    this.manager.context._canplay = true
+    this.manager.context.canplay.emit()
+  }
+  this.add.emit(el)
+}
+
+// When call stop method，need free up resource
+function close(this: Player) {
+  if (this.playing()) {
+    this.el?.pause()
+  }
+  // We don't call context close, because that's a single audio
+  this.manager.context.disconnect()
+  this.close.emit()
+}
+
+function play(this: Player) {}
+
+function pause(this: Player) {}
+
+function volume(this: Player, val: number) {
+  if (__DEV__) {
+    assert(typeof val === 'number' && !Number.isNaN(val), 'Not a legal value.')
   }
 }
 
-function playError(player: Player) {
-  return (error: Error) => {
-    warn(error.message)
-    player.add.emit(error)
-    return false
+function mute(this: Player, val: boolean) {
+  if (__DEV__) {
+    assert(typeof val === 'boolean', 'Not a legal value.')
   }
+}
+
+function forward(this: Player, val: number) {
+  if (__DEV__) {
+    assert(typeof val === 'number', 'Not a legal value.')
+  }
+}
+
+function playing(this: Player) {
+  return this.el ? ((this.el as unknown) as HTMLAudioElement).paused : false
 }
 
 export function Long(manager: Manager, options?: StartOptions): Player {
   const Player = {
     manager,
+    options,
     el: null,
-
-    // Use audio element load source and control audio behavior
-    add: extend(function (this: Player, url: string) {
-      if (__DEV__) {
-        assert(typeof url === 'string', 'error')
-      }
-      const el = (this.el = options?.audio || new Audio())
-
-      if (options?.crossOrigin) {
-        el.crossOrigin = options.crossOrigin
-      }
-      el.src = url
-      el.loop = Boolean(options?.loop)
-      el.oncanplay = () => {
-        // Alter context canplay attribute and dispatch event
-        manager.context._canplay = true
-        manager.context.canplay.emit()
-      }
-    }),
-
-    // When call stop method，need free up resource
-    close: extend(function (this: Player) {}),
-
-    playing() {
-      return this.el ? ((this.el as unknown) as HTMLAudioElement).paused : false
-    },
+    playing,
+    add: extend(add),
+    mute: extend(mute),
+    play: extend(play),
+    close: extend(close),
+    pause: extend(pause),
+    volume: extend(volume),
+    forward: extend(forward),
   }
 
   return Player
